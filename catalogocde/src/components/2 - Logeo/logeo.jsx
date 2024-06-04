e6be689824373aa7ff5ba948fabc35187508a7c2
CamiloMoraGDW <cmora@godoworks.com>
2024-06-04 15:33:34 -0300

    "jfjjjfjdjdjd"

--- catalogocde/src/components/2 - Logeo/logeo.jsx
+++ catalogocde/src/components/2 - Logeo/logeo.jsx
@@ -1,115 +1,3 @@
-// import React, { useState } from "react";
-// import logo from "../../recursos/LogoBlancoSinFondo.png";
-// import "./logeo.css";
-// import firebaseApp from "../../../credenciales";
-// import {
-//     getAuth,
-//     createUserWithEmailAndPassword,
-//     signInWithEmailAndPassword,
-//     signInWithRedirect,
-//     GoogleAuthProvider
-// } from "firebase/auth";
-
-// const auth = getAuth(firebaseApp);
-
-// const Logeo = () => {
-//     const [estaRegistrandose, setEstaRegistrandose] = useState(false);
-//     const [email, setEmail] = useState("");
-//     const [password, setPassword] = useState("");
-//     const [error, setError] = useState("");
-
-//     const handleChange = (e) => {
-//         const { name, value } = e.target;
-//         if (name === "email") {
-//             setEmail(value);
-//         } else if (name === "password") {
-//             setPassword(value);
-//         }
-//     };
-
-//     async function submitHandler(e) {
-//         e.preventDefault();
-//         setError(""); // Resetea el mensaje de error al intentar de nuevo
-
-//         try {
-//             if (estaRegistrandose) {
-//                 const usuario = await createUserWithEmailAndPassword(auth, email, password);
-//                 console.log(usuario);
-//             } else {
-//                 const usuario = await signInWithEmailAndPassword(auth, email, password);
-//                 console.log(usuario);
-//             }
-//         } catch (error) {
-//             // Manejo de errores y mostrar mensajes de alerta
-//             switch (error.code) {
-//                 case "auth/email-already-in-use":
-//                     setError("El correo electrónico ya está en uso.");
-//                     break;
-//                 case "auth/invalid-email":
-//                     setError("El correo electrónico no es válido.");
-//                     break;
-//                 case "auth/weak-password":
-//                     setError("La contraseña es demasiado débil.");
-//                     break;
-//                 case "auth/user-not-found":
-//                     setError("No se encontró ninguna cuenta con este correo electrónico.");
-//                     break;
-//                 case "auth/wrong-password":
-//                     setError("La contraseña es incorrecta.");
-//                     break;
-//                 default:
-//                     setError("Ocurrió un error inesperado. Por favor, inténtalo de nuevo.");
-//                     break;
-//             }
-//         }
-//     }
-
-//     return (
-//         <>
-//             <div className="container">
-//                 <div className="left-side">
-//                     <div className="content">
-//                         <div className="logoGDW">
-//                             <img src={logo} alt="" />
-//                         </div>
-//                         <div className="texto">
-//                             <p>Bienvenido/a al catálogo de Casos de Éxito Godoworks! Este sitio tiene como objetivo asistir a los comerciales a buscar y explorar los Casos de Éxito elaborados por la empresa. Esta solución ayuda a los comerciales a mejorar sus demos y prospección. Por favor {estaRegistrandose ? "crea una cuenta" : "ingresa tu cuenta"} y disfruta del catálogo CDE.</p>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 <div className="right-side">
-//                     <div className="form-box">
-//                         <form onSubmit={submitHandler}>
-//                             <h1>{estaRegistrandose ? "Registrate" : "Iniciar Sesion"}</h1>
-//                             <div className="input-box">
-//                                 <input type="email" placeholder="Correo electronico" id="email" name="email" value={email} onChange={handleChange} />
-//                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-//                                     <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-//                                 </svg>
-//                             </div>
-//                             <div className="input-box">
-//                                 <input type="password" placeholder="Contraseña" id="password" name="password" value={password} onChange={handleChange} />
-//                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-//                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
-//                                 </svg>
-//                             </div>
-//                             {error && <div className="error-message">{error}</div>}
-//                             <div className="register-button">
-//                                 <button type="submit">{estaRegistrandose ? "Registrarme" : "Iniciar Sesion"}</button>
-//                             </div>
-//                         </form>
-//                             <button onClick={() => setEstaRegistrandose(!estaRegistrandose)} className="switch-form">{estaRegistrandose ? "Ya tengo una cuenta" : "No tengo una cuenta, registrarme."}</button>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
-
-// export default Logeo;
-
-
-
 import React, { useState } from "react";
 import logo from "../../recursos/LogoBlancoSinFondo.png";
 import "./logeo.css";
@@ -122,7 +10,6 @@ import {
 } from "firebase/auth";
 
 const auth = getAuth(firebaseApp);
-const googleProvider = new GoogleAuthProvider();
 
 const Logeo = () => {
     const [estaRegistrandose, setEstaRegistrandose] = useState(false);
@@ -144,8 +31,7 @@ const Logeo = () => {
 
     async function submitHandler(e) {
         e.preventDefault();
-        setError(""); // Resetea el mensaje de error al intentar de nuevo
-
+        setError("");
         if (estaRegistrandose && password !== confirmPassword) {
             setError("Las contraseñas no coinciden.");
             return;
@@ -187,7 +73,7 @@ const Logeo = () => {
     return (
         <>
             <div className="container">
-                <div className="left-side">
+                <div className="log-left-side">
                     <div className="content">
                         <div className="logoGDW">
                             <img src={logo} alt="" />
@@ -197,7 +83,7 @@ const Logeo = () => {
                         </div>
                     </div>
                 </div>
-                <div className="right-side">
+                <div className="log-right-side">
                     <div className="form-box">
                         <form onSubmit={submitHandler}>
                             <h1>{estaRegistrandose ? "Registrate" : "Iniciar Sesion"}</h1>
--- catalogocde/src/components/3 - ListadoCDE/listadoCDE.jsx
+++ catalogocde/src/components/3 - ListadoCDE/listadoCDE.jsx
@@ -5,7 +5,6 @@ import './ListadoCDE.css';
 
 const ListadoCDE = () => {
     const [cdes, setCdes] = useState([]);
-    const [originalCdes, setOriginalCdes] = useState([]); // Nueva variable para almacenar la lista original
     const [searchTerm, setSearchTerm] = useState('');
     const [editCde, setEditCde] = useState(null);
     const [updatedData, setUpdatedData] = useState({
@@ -15,13 +14,15 @@ const ListadoCDE = () => {
         creationDate: '',
     });
     const [isSaving, setIsSaving] = useState(false);
+    const [showConfirmPopup, setShowConfirmPopup] = useState(false); // Estado para controlar el popup
+    const [cdeToDelete, setCdeToDelete] = useState(null); // Estado para almacenar el CDE a eliminar
+    const [isDeleting, setIsDeleting] = useState(false); // Estado para controlar el indicador de carga
 
     useEffect(() => {
         const fetchCdes = async () => {
             const querySnapshot = await getDocs(collection(firestore, 'cdes'));
             const cdeList = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
             setCdes(cdeList);
-            setOriginalCdes(cdeList); // Almacena la lista original
         };
 
         fetchCdes();
@@ -29,7 +30,7 @@ const ListadoCDE = () => {
 
     const handleSearch = () => {
         const lowercasedSearchTerm = searchTerm.toLowerCase();
-        const filteredCdes = originalCdes.filter(cde => {
+        const filteredCdes = cdes.filter(cde => {
             const lowercasedName = cde.name.toLowerCase();
             const lowercasedClient = cde.client.toLowerCase();
             const lowercasedCountry = cde.country.toLowerCase();
@@ -70,15 +71,28 @@ const ListadoCDE = () => {
         setIsSaving(false);
     };
 
-    const handleDelete = async (id) => {
+    const handleDelete = (id) => {
+        setCdeToDelete(id);
+        setShowConfirmPopup(true);
+    };
+
+    const confirmDelete = async () => {
+        setIsDeleting(true); // Mostrar el indicador de carga
         try {
-            await deleteDoc(doc(firestore, 'cdes', id));
-            setCdes(cdes.filter(cde => cde.id !== id));
-            setOriginalCdes(originalCdes.filter(cde => cde.id !== id)); // Actualiza la lista original
+            await deleteDoc(doc(firestore, 'cdes', cdeToDelete));
+            setCdes(cdes.filter(cde => cde.id !== cdeToDelete));
+            setShowConfirmPopup(false);
+            setCdeToDelete(null);
         } catch (error) {
             console.error('Error eliminando el CDE:', error);
             alert('Hubo un error eliminando el CDE.');
         }
+        setIsDeleting(false); // Ocultar el indicador de carga
+    };
+
+    const cancelDelete = () => {
+        setShowConfirmPopup(false);
+        setCdeToDelete(null);
     };
 
     return (
@@ -92,35 +106,27 @@ const ListadoCDE = () => {
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <button onClick={handleSearch}>Buscar</button>
-                <button onClick={() => {
-                    setSearchTerm(''); // Restablece el término de búsqueda
-                    setCdes(originalCdes); // Restaura la lista original
-                }}>Limpiar</button>
             </div>
             <div className="cde-list">
                 {cdes.map((cde) => (
                     <div key={cde.id} className="cde-item">
                         {editCde && editCde.id === cde.id ? (
                             <div>
-                                <p><b>Nombre:</b></p>
                                 <input
                                     type="text"
                                     value={updatedData.name}
                                     onChange={(e) => setUpdatedData({ ...updatedData, name: e.target.value })}
                                 />
-                                <p><b>Cliente:</b></p>
                                 <input
                                     type="text"
                                     value={updatedData.client}
                                     onChange={(e) => setUpdatedData({ ...updatedData, client: e.target.value })}
                                 />
-                                <p><b>Pais:</b></p>
                                 <input
                                     type="text"
                                     value={updatedData.country}
                                     onChange={(e) => setUpdatedData({ ...updatedData, country: e.target.value })}
                                 />
-                                <p><b>Fecha de alta:</b></p>
                                 <input
                                     type="date"
                                     value={updatedData.creationDate}
@@ -133,21 +139,35 @@ const ListadoCDE = () => {
                             </div>
                         ) : (
                             <div>
-                                <h2>{cde.name}</h2>
+                                <h2>
+                                    <a href={cde.link} target="_blank" rel="noopener noreferrer">{cde.name}</a>
+                                </h2>
                                 <p><strong>Cliente:</strong> {cde.client}</p>
                                 <p><strong>País:</strong> {cde.country}</p>
                                 <p><strong>Fecha de creación:</strong> {cde.creationDate}</p>
-                                <h4>
-                                    <a href={cde.link} target="_blank" rel="noopener noreferrer">PDF</a>
-                                </h4>
                                 <button onClick={() => handleEdit(cde)}>Editar</button>
                                 <button onClick={() => handleDelete(cde.id)}>Eliminar</button>
-                                
                             </div>
                         )}
                     </div>
                 ))}
             </div>
+
+            {showConfirmPopup && (
+                <div className="popup-overlay">
+                    <div className="popup">
+                        <h3>¿Está seguro que desea eliminar este CDE?</h3>
+                        {isDeleting ? (
+                            <p>Cargando...</p> // Mostrar "Cargando..." mientras se elimina
+                        ) : (
+                            <>
+                                <button onClick={confirmDelete}>Sí, eliminar</button>
+                                <button onClick={cancelDelete}>Cancelar</button>
+                            </>
+                        )}
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
